bd982ceec25d962c91a4538e2b990ff557a095de
Jae Hun Lee <ljhjohn1@gmail.com>
2022-11-16 11:42:11 +0900

    Initial Commit

---
+++ JaeHunApp/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JaeHunApp
+//
+//  Created by 이재훈 on 2022/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
